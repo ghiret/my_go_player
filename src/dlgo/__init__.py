2375113af47edc9f4637959b415bdd8c0b28d6d9
Paco Cordobes <ghiret@gmail.com>
2024-08-07 23:01:22 +0000

    Add Player, Point, Move, GoString and Board classes.
    Include some basic tests for Player and Point.
    
    Remove the License as the code is copied from the book.

---
+++ src/dlgo/__init__.py
@@ -0,0 +1,1 @@
+# src/dlgo/__init__.py
---
+++ tests/test_gotypes.py
@@ -0,0 +1,20 @@
+# test_dlgo.py
+
+from dlgo.go_types import Player, Point
+
+def test_player_black_other():
+    assert Player.black.other == Player.white
+
+def test_player_white_other():
+    assert Player.white.other == Player.black
+
+
+def test_point_neighbors():
+    p = Point(2, 3)
+    expected_neighbors = [
+        Point(1, 3),
+        Point(3, 3),
+        Point(2, 2),
+        Point(2, 4)
+    ]
+    assert p.neighbors() == expected_neighbors
