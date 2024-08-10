46a47989160264d91e89b72be315e06e935f90e9
Paco Cordobes <ghiret@gmail.com>
2024-08-10 13:05:59 +0000

    Re-structure the tests folder to mimic the structure of the src folder in anticipation of the large amount of code in this project.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
+import os
 import sys
 from pathlib import Path
 
-# Add the src directory to the Python path
-src_path = Path(__file__).parent / "src"
-sys.path.insert(0, str(src_path))
+# Get the project root directory
+root_dir = Path(__file__).parent
+
+# Add the project root and src directories to Python path
+sys.path.insert(0, str(root_dir))
+sys.path.insert(0, str(root_dir / "src"))
+sys.path.insert(0, str(root_dir / "tests"))
+
+print("Python path in conftest:", sys.path)
