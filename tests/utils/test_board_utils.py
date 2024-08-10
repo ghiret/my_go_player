4b44d9eedcf558a11cd5610936b1f688cee3930a
Paco Cordobes <ghiret@gmail.com>
2024-08-10 14:10:26 +0100

    Re-structure the tests folder to mimic the structure of the src folde… (#6)
    * Re-structure the tests folder to mimic the structure of the src folder in anticipation of the large amount of code in this project.
    
    * chore: Update content-hash in poetry.lock

--- tests/utils/test_board_utils.py
+++ tests/utils/test_board_utils.py
@@ -1,11 +1,10 @@
 from contextlib import redirect_stdout
 from io import StringIO
-
 import pytest
 
 from dlgo.goboard_slow import Board
 from dlgo.gotypes import Player, Point
-from utils.board_utils import create_board_from_ascii, print_board
+from src.utils.board_utils import create_board_from_ascii, print_board
 
 
 def test_create_empty_board():
