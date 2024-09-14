b0c15649410145c633b4af1292c5ae0acf1ae6fe
Paco Cordobes <ghiret@gmail.com>
2024-09-14 09:40:51 +0100

    Pre-commit and only install tensorflow-macos and tensorflow-metal on Mac.

--- src/examples/mcts_go_cnn.py
+++ src/examples/mcts_go_cnn.py
@@ -10,13 +10,12 @@ from __future__ import print_function
 
 # tag::mcts_go_cnn_preprocessing[]
 import numpy as np
+from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 
 np.random.seed(123)
-X = np.load('src/dlgo/generated_games/features-40k.npy')
-Y = np.load('src/dlgo/generated_games/labels-40k.npy')
+X = np.load("src/dlgo/generated_games/features-40k.npy")
+Y = np.load("src/dlgo/generated_games/labels-40k.npy")
 
 samples = X.shape[0]
 size = 9
@@ -31,53 +30,121 @@ Y_train, Y_test = Y[:train_samples], Y[train_samples:]
 
 # tag::mcts_go_cnn_model[]
 model = Sequential()
-model.add(Conv2D(48, kernel_size=(3, 3),
-                 activation='relu',
-                 padding='same',
-                 input_shape=input_shape))
+model.add(Conv2D(48, kernel_size=(3, 3), activation="relu", padding="same", input_shape=input_shape))
 model.add(Dropout(rate=0.5))
-model.add(Conv2D(48, (3, 3),
-                 padding='same', activation='relu'))
+model.add(Conv2D(48, (3, 3), padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(rate=0.5))
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
+model.add(Dense(512, activation="relu"))
 model.add(Dropout(rate=0.5))
-model.add(Dense(size * size, activation='softmax'))
+model.add(Dense(size * size, activation="softmax"))
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+model.compile(loss="categorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
 # end::mcts_go_cnn_model[]
 
 # tag::mcts_go_cnn_eval[]
-model.fit(X_train, Y_train,
-          batch_size=64,
-          epochs=400,
-          verbose=1,
-          validation_data=(X_test, Y_test))
+model.fit(X_train, Y_train, batch_size=64, epochs=400, verbose=1, validation_data=(X_test, Y_test))
 score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
 # end::mcts_go_cnn_eval[]
 
-test_board = np.array([[[
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 1, -1,  1, -1, 0, 0, 0, 0,
-    0, 1, -1,  1, -1, 0, 0, 0, 0,
-    0, 0,  1, -1,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-    0, 0,  0,  0,  0, 0, 0, 0, 0,
-]]]).reshape(1, 9, 9, 1)
+test_board = np.array(
+    [
+        [
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                -1,
+                1,
+                -1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                -1,
+                1,
+                -1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                1,
+                -1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ]
+        ]
+    ]
+).reshape(1, 9, 9, 1)
 move_probs = model.predict(test_board)[0]
 i = 0
 for row in range(9):
     row_formatted = []
     for col in range(9):
-        row_formatted.append('{:.3f}'.format(move_probs[i]))
+        row_formatted.append("{:.3f}".format(move_probs[i]))
         i += 1
-    print(' '.join(row_formatted))
+    print(" ".join(row_formatted))
